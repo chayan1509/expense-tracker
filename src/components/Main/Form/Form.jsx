e41734375bcef113c988e90bf344ea91edfd2e8b
Chayan Mukherjee <chayanru99@gmail.com>
2022-09-06 20:47:20 +0530

    context

--- src/components/Main/Form/Form.jsx
+++ src/components/Main/Form/Form.jsx
@@ -30,6 +30,22 @@ const Form = () => {
           </Select>
         </FormControl>
       </Grid>
+      <Grid item xs={6}>
+        <FormControl fullWidth>
+            <InputLabel>Category</InputLabel>
+            <Select>
+                <MenuItem value="business"> Business </MenuItem>
+                <MenuItem value="salary"> Salary </MenuItem>
+            </Select>
+        </FormControl>
+      </Grid>
+      <Grid item xs={6}>
+        <TextField type="number" label="Amount" fullWidth/>
+      </Grid>
+      <Grid item xs={6}>
+        <TextField type="date" label="Date" fullWidth/>
+      </Grid>
+      <Button className={classes.button} variant="outlined" color="primary" fullWidth>Create</Button>
     </Grid>
   );
 };
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -8,6 +8,8 @@ import {
   Divider,
 } from "@material-ui/core";
 import useStyles from "./styles";
+import Form from "./Form/Form";
+import List from "./List/List";
 
 const Main = () => {
   const classes = useStyles();
@@ -23,15 +25,16 @@ const Main = () => {
           variant="subtitle1"
           style={{ lineHeight: "1.5em", marginTop: "20px" }}
         >
-          
           {/* Info */}
         </Typography>
         <Divider />
         {/* Form */}
+        <Form/>
         <CardContent className={classes.cartContent}>
           <Grid container spacing={2}>
             <Grid item xs={12}>
               {/* List */}
+              <List/>
             </Grid>
           </Grid>
         </CardContent>
