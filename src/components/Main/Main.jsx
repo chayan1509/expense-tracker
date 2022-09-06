b8c58bb184ba9499392ed50a57aa58fb3f8b23c3
Chayan Mukherjee <chayanru99@gmail.com>
2022-09-06 19:52:54 +0530

    FORM, CARDS, MAIN

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,43 @@
+import React, { useState, useEffect, useContext } from "react";
+import {
+  Card,
+  CardHeader,
+  CardContent,
+  Typography,
+  Grid,
+  Divider,
+} from "@material-ui/core";
+import useStyles from "./styles";
+
+const Main = () => {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root}>
+      <CardHeader title="Expense Tracker"   subheader="For Personal Use Only" />
+      <CardContent>
+        <Typography align="center" variant="h5">
+          Total Balance ₹100
+        </Typography>
+        <Typography
+          variant="subtitle1"
+          style={{ lineHeight: "1.5em", marginTop: "20px" }}
+        >
+          
+          {/* Info */}
+        </Typography>
+        <Divider />
+        {/* Form */}
+        <CardContent className={classes.cartContent}>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              {/* List */}
+            </Grid>
+          </Grid>
+        </CardContent>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default Main;
