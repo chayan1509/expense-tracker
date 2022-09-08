e450ecae9fac2bca126a1b9a9a92ae8a8ad36208
Chayan Mukherjee <chayanru99@gmail.com>
2022-09-08 15:48:13 +0530

    Final

--- src/components/Main/List/List.jsx
+++ src/components/Main/List/List.jsx
@@ -1,18 +1,9 @@
-import React, { useContext } from "react";
-import {
-  List as MUIList,
-  ListItem,
-  ListItemAvatar,
-  Avatar,
-  ListItemText,
-  ListItemSecondaryAction,
-  IconButton,
-  Slide,
-} from "@material-ui/core";
-import { Delete, MoneyOff } from "@material-ui/icons";
+import React, { useContext } from 'react';
+import { List as MUIList, ListItem, ListItemAvatar, Avatar, ListItemText, ListItemSecondaryAction, IconButton, Slide } from '@material-ui/core';
+import { Delete, MoneyOff } from '@material-ui/icons';
 
-import { ExpenseTrackerContext } from "../../../context/context";
-import useStyles from "./styles";
+import { ExpenseTrackerContext } from '../../../context/context';
+import useStyles from './styles';
 
 const List = () => {
   const classes = useStyles();
@@ -21,35 +12,16 @@ const List = () => {
   return (
     <MUIList dense={false} className={classes.list}>
       {transactions.map((transaction) => (
-        <Slide
-          direction="down"
-          in
-          mountOnEnter
-          unmountOnExit
-          key={transaction.id}
-        >
+        <Slide direction="down" in mountOnEnter unmountOnExit key={transaction.id}>
           <ListItem>
             <ListItemAvatar>
-              <Avatar
-                className={
-                  transaction.type === "Income"
-                    ? classes.avatarIncome
-                    : classes.avatarExpense
-                }
-              >
+              <Avatar className={transaction.type === 'Income' ? classes.avatarIncome : classes.avatarExpense}>
                 <MoneyOff />
               </Avatar>
             </ListItemAvatar>
-            <ListItemText
-              primary={transaction.category}
-              secondary={`$${transaction.amount} - ${transaction.date}`}
-            />
+            <ListItemText primary={transaction.category} secondary={`₹${transaction.amount} - ${transaction.date}`} />
             <ListItemSecondaryAction>
-              <IconButton
-                edge="end"
-                aria-label="delete"
-                onClick={() => deleteTransaction(transaction.id)}
-              >
+              <IconButton edge="end" aria-label="delete" onClick={() => deleteTransaction(transaction.id)}>
                 <Delete />
               </IconButton>
             </ListItemSecondaryAction>
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,46 +1,35 @@
-import React, { useState, useEffect, useContext } from "react";
-import {
-  Card,
-  CardHeader,
-  CardContent,
-  Typography,
-  Grid,
-  Divider,
-} from "@material-ui/core";
-import useStyles from "./styles";
-import Form from "./Form/Form";
-import List from "./List/List";
+import React, { useContext } from 'react';
+import { Card, CardHeader, CardContent, Typography, Grid, Divider } from '@material-ui/core';
+import { ExpenseTrackerContext } from '../../context/context';
+import useStyles from './styles';
+import Form from './Form/Form';
+import List from './List/List';
+import InfoCard from '../InfoCard';
 
-const Main = () => {
+const ExpenseTracker = () => {
   const classes = useStyles();
+  const { balance } = useContext(ExpenseTrackerContext);
 
   return (
     <Card className={classes.root}>
-      <CardHeader title="Expense Tracker"   subheader="For Personal Use Only" />
+      <CardHeader title="Expense Tracker" subheader="Chayan Mukherjee" />
       <CardContent>
-        <Typography align="center" variant="h5">
-          Total Balance ₹100
+        <Typography align="center" variant="h5">Total Balance ₹{balance}</Typography>
+        <Typography variant="subtitle1" style={{ lineHeight: '1.5em', marginTop: '20px' }}>
+          <InfoCard />
         </Typography>
-        <Typography
-          variant="subtitle1"
-          style={{ lineHeight: "1.5em", marginTop: "20px" }}
-        >
-          {/* Info */}
-        </Typography>
-        <Divider />
-        {/* Form */}
-        <Form/>
-        <CardContent className={classes.cartContent}>
-          <Grid container spacing={2}>
-            <Grid item xs={12}>
-              {/* List */}
-              <List/>
-            </Grid>
+        <Divider className={classes.divider} />
+        <Form />
+      </CardContent>
+      <CardContent className={classes.cartContent}>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <List />
           </Grid>
-        </CardContent>
+        </Grid>
       </CardContent>
     </Card>
   );
 };
 
-export default Main;
+export default ExpenseTracker;
